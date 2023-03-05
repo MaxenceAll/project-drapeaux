135544599afce5c63f7861d3b2c07dca3f2fab46
MaxenceAll <maxence.allart@gmail.com>
2023-03-05 23:44:02 +0100

    filtre par continet

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,6 +5,7 @@ import Card from "./Card";
 const Countries = () => {
   const [data, setData] = useState([]);
   const [rangeValue, setRangeValue] = useState(36);
+  const [selectedRadio, setSelectedRadio] = useState("");
 
   const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
@@ -27,13 +28,30 @@ const Countries = () => {
           />
         {radios.map( (continent) => (
             <li>
-                <input type="radio" id={continent} />
+                <input 
+                  type="radio" 
+                  id={continent}
+                  name="continentRadio"
+                  checked={continent===selectedRadio}
+                  onChange={(e)=>setSelectedRadio(e.target.id)}
+                  key={continent}
+                />
                 <label htmlFor={continent}> {continent} </label>
             </li>
         ))}
         </ul>
+
+          {selectedRadio && 
+          <button
+            onClick={()=>setSelectedRadio("")}
+          >Reset search</button>}
+
         <ul>
-          {data.slice(0, rangeValue).map((country, index) => (
+          {data
+          .filter( (country) => country.continents[0].includes(selectedRadio))
+          .slice(0, rangeValue)
+          .sort( (a,b) => b.population - a.population )
+          .map((country, index) => (
             <Card key={index} country={country} />
           ))}
         </ul>
